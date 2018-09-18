31ca7e3729448de93c4d967e9c58410bee17803b
kirillkolesnik123 <42231774+kirillkolesnik123@users.noreply.github.com>
2018-09-19 00:36:51 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+    //задали функцию сортировки массива от наименьшего к наибольшему
+  function forsort(a,b)
+  {return a - b};
+  //условие для length =1 or 0
+    if(array.length === 0)
+        return 0;
+    if(array.length === 1)
+        return 1;
+
+  let count = 1;
+  let fortest =0;
+  array.sort(forsort);
+  //cycle for элементов ,которые идут попорядку
+  for(let i=1; i<array.length;i++){
+    if(array[i]-array[i-1] === 0)
+        {continue;}
+    if(array[i]-array[i-1] === 1)
+        {count++;}
+    if(array[i]-array[i-1] > 1){
+        fortest = (count>fortest)? count :fortest;
+            count=1;//обновляем счет Consecutivelength ,когда в массиве обрывается первый порядок чисел.
+  }
+}
+    return fortest; 
 }
